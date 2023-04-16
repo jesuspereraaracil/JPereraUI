4c1355bb40d0d23775012e660b835258ab8e12be
git_username <git_email>
2023-04-16 11:35:40 +0200

    chore(lib): removing lib from git

--- lib/atoms/TextInput/TextInput.d.ts
+++
@@ -1,9 +0,0 @@
-import React, { ChangeEventHandler } from 'react';
-export interface TextInputProps {
-    disabled?: boolean;
-    value?: string;
-    onChangeHandler?: ChangeEventHandler;
-    placeholder?: string;
-    className?: string;
-}
-export declare const TextInput: React.FC<TextInputProps>;
--- lib/theme/spacing.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const spacing: {
-    TINY: string;
-    SMALL: string;
-    NORMAL: string;
-    BIG: string;
-    HUGE: string;
-};
