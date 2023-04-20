7aafc47559e98eddc004b906c9b2ed90d914d1e6
git_username <git_email>
2023-04-20 19:47:55 +0200

    chores(removing types folder)

--- types/atoms/TextInput/TextInput.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react" />
-import { TextInputProps } from './TextInput.types';
-export declare const TextInput: ({ disabled, value, onChangeHandler, placeholder, className }: TextInputProps) => JSX.Element;
--- types/atoms/TextInput/TextInput.styled.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-import { TextInputProps } from './TextInput.types';
-export declare const TextInputStyled: import("@emotion/styled").StyledComponent<{
-    theme?: import("@emotion/react").Theme | undefined;
-    as?: import("react").ElementType<any> | undefined;
-} & TextInputProps, import("react").DetailedHTMLProps<import("react").InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, {}>;
