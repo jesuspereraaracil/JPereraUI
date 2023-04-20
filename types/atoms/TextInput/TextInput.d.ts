d35f72343d5ee19050d4cb21ab7e6ec2ff439e44
git_username <git_email>
2023-04-20 19:46:26 +0200

    chores(cleaning up build stage)

---
+++ types/atoms/TextInput/TextInput.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="react" />
+import { TextInputProps } from './TextInput.types';
+export declare const TextInput: ({ disabled, value, onChangeHandler, placeholder, className }: TextInputProps) => JSX.Element;
