2a137db7017424cb4ffd7506ef842c9082528eff
git_username <git_email>
2023-05-03 17:16:46 +0200

    chore(clean code)

--- src/atoms/Button/Button.types.ts
+++ src/atoms/Button/Button.types.ts
@@ -1,18 +1,7 @@
 import { MouseEventHandler } from 'react';
 
-// normal, danger, warning, disabled
-// outline normal, danger, warning, disabled
-// ghost normal, danger, warning, disabled
-
-export enum ButtonType {
-  PRIMARY,
-  SECONDARY,
-  WARNING,
-  DANGER,
-}
 export interface ButtonProps {
   disabled: boolean;
   onClick: MouseEventHandler<HTMLButtonElement>;
   text: string;
-  type: ButtonType;
 }
