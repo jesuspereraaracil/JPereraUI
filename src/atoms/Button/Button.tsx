7d0d9589b6777ddd24b3e547ded92a04e5bd2e05
git_username <git_email>
2023-07-14 17:00:48 +0200

    feat(removing container)

--- src/atoms/Button/Button.tsx
+++ src/atoms/Button/Button.tsx
@@ -1,13 +1,10 @@
 import { ButtonProps } from './Button.types';
 import { ButtonStyled } from './Button.styled';
 import React from 'react';
-import { Container } from '../Container/Container.styled';
 
 export const Button = (props: ButtonProps): React.JSX.Element => {
   const { text } = props;
   return (
-    <Container>
-      <ButtonStyled {...props}>{text}</ButtonStyled>
-    </Container>
+    <ButtonStyled {...props}>{text}</ButtonStyled>
   );
 };
